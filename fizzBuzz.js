20c7c22c86223407cd5faf2228e19624c8e3d202
JoseSNunes <josesimasnunes@gmail.com>
2023-03-15 13:57:58 +0000

    Solution: jose simao duarte sinde

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,6 +1,20 @@
-const fizzBuzz = () => {
+const fizzBuzz = (number) => {
   // Start here
+  if (number % 3 === 0 && number % 5 === 0){
+    return "fizzbuzz";
+  } else if (number % 5 === 0){
+    return "buzz";
+  } else if ((number % 3 === 0) ){
+    return "fizz";
+  } else{
+    return number;
+  }
 };
+
+// % 3 == 0
+// % 5 == 0
+// % 3 == 0 && % 5 == 0 
+// return number
 
 // Check your console.log on the browser console and match all outputs
 console.log(fizzBuzz(3)); // fizz
