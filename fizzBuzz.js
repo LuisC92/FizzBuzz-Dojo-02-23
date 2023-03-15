55e8f82d5d4ba884782d102bf430d7b1ad5fc411
Tomás Botelho Moniz <tbotelhomonizlg@gmail.com>
2023-03-15 14:28:09 +0000

    Finished the first exercise

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,6 +1,16 @@
-const fizzBuzz = () => {
-  // Start here
-};
+const fizzBuzz = (number) => {
+    if (number % 3 === 0  && number % 5 === 0) {
+        return "fizzBuzz"
+     } else if (number % 3 === 0) {
+        return "fizz" 
+     } else if (number % 5 === 0) {
+        return "buzz"
+     }
+       else {
+        return number
+       }
+     }
+
 
 // Check your console.log on the browser console and match all outputs
 console.log(fizzBuzz(3)); // fizz
